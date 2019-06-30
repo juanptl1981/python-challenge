29fb622b9aa0e367d7790d5777399f68bd46d345
juanptl1981 <juanptlr@gmail.com>
2019-06-29 22:22:24 -0500

    Remove - Output code.
    I had to remove the output code to be able to run in the terminal without errors.

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,9 +10,6 @@ with open(csv_path, newline="") as csvfile:
     profit_loss = []
     total_month = []
     profit_loss_change = []
-    financial_Analysis = "Financial Analysis/n "
-    output_path = "output_1.txt"
-
 
     for row in reader:
         profit_loss.append(float(row[1]))
@@ -35,16 +32,13 @@ with open(csv_path, newline="") as csvfile:
         max_profit_loss_change_date = str(total_month[profit_loss_change.index(max(profit_loss_change))])
         
         min_profit_loss_change_date = str(total_month[profit_loss_change.index(min(profit_loss_change))])
-    
-#with open("output.txt", "W") as f:
-        print("Financial Analysis", file=f))
-        print("------------------", file=f)
-        print(f"Total Months: {len(total_month)}", file=f)
-        print(f"Total Profit/Loss: $ {sum(profit_loss)}", file=f)
-        print(f"Average Profit/loss Change: $ {round(avg_profit_loss_change)}", file=f)
-        print(f"Greatest Increase in Profits:{max_profit_loss_change_date} - $ {max_profit_loss_change}", file=f)
-        print(f"Greatest Decrease in Profits:{min_profit_loss_change_date} - $ {min_profit_loss_change}", file=f)
-        print("------------------", file=f)
 
-       
+
+    print("Financial Analysis")
+    print("------------------")
+    print(f"Total Months: {len(total_month)}")
+    print(f"Total Profit/Loss: $ {sum(profit_loss)}")
+    print(f"Average Profit/loss Change: $ {round(avg_profit_loss_change)}")
+    print(f"Greatest Increase in Profits:{max_profit_loss_change_date} - $ {max_profit_loss_change}")
+    print(f"Greatest Decrease in Profits:{min_profit_loss_change_date} - $ {min_profit_loss_change}")
 
